1a07c1f704f362dbbf80c3f71896312abb45727f
Adnan Malik <adnanmalikinfo110@gmail.com>
2021-01-04 23:30:31 -0800

    updated

--- linkExtractor.py
+++ linkExtractor.py
@@ -1,4 +1,5 @@
 #! python3
+
 import re
 import pyperclip
 
@@ -12,6 +13,8 @@ linksRegex = re.compile(r'''
 randomData = pyperclip.paste()
 mo = linksRegex.findall(randomData)
 
-clearFormat = '\n'.join(mo)
-pyperclip.copy(clearFormat)
+clearFormat = '\n'.join(mo) #it will make it in nice format
+
+pyperclip.copy(clearFormat)  #your clipboard have been updated with the result of the program
+
 print(clearFormat)
